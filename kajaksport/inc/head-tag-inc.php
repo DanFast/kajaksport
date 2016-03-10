6d7c81af15e3d8a929aad0f4ee62c5d7517f4e33
Peter Wendt <peter.wendt@liwest.at>
2016-03-10 15:11:55 +0100

    icon changed

--- kajaksport/inc/head-tag-inc.php
+++ kajaksport/inc/head-tag-inc.php
@@ -18,4 +18,9 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <!--Setzen des Icons Hallllllllloooasdas -->
+
+    <link rel="shortcut icon" href="img/LogoWFV.png">
+
 </head>
